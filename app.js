24712c3ea7d52e0c06858aa92b66c5abceb82373
miaravnanger <mia_maroen@hotmail.com>
2025-10-30 14:45:19 +0100

    new start

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 const breedSelect = document.getElementById("breed");
-const gallery = document.getElementById("gallery");
-const loadBtn = document.getElementById("loadCats");
+const gallery = document.getElementById("images");
+
+// To do: 
+// fetch list of all breeds and put them into form select
+
+// fetch images
+fetch ("")
+    
+// event listener på knapp 
+const loadBtn = document.getElementById("loadDog").addEventListener("click", (e) => {
+
+})
